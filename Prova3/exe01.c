27afaa79a1aac8488555ab1ffffc2c888f5c4e0f
kamillyscholze <134938384+kamillyscholze@users.noreply.github.com>
2023-05-29 10:29:32 -0300

    prova3 - exe01

---
+++ Prova3/exe01.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+
+    int N, K, i, mult;
+
+    do
+    {
+        printf("Digite N e K positivos: ");
+        scanf("%d %d", &N, &K);
+
+        if ((N < 1) || (K < 1) || (K < N))
+        {
+            printf ("Invalido!\n");
+        }
+        
+    } while ((N < 1) || (K < 1) || (K < N));
+
+    for (i = 1; i <= K; i++) 
+    {                           
+        mult = N * i;
+                           
+        printf("\n%d * %d = %d", N, i, mult);
+    }
+    
+    return 0;
+}
+// 1. Leia N e K positivos, para K > N, e imprima a tabuada de N para todo i de 1 a K.
